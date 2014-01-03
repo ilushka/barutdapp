bca397d09e04451de9e40988e2c77d8f95dc93ee
ilya moskovko <ilya@iswifter.net>
2014-01-03 00:10:21 -0800

    added action bar

--- src/com/moskovko/barutdapp/MainActivity.java
+++ src/com/moskovko/barutdapp/MainActivity.java
@@ -1,19 +1,20 @@
 package com.moskovko.barutdapp;
 
-import android.app.Activity;
+//import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.view.Window;
 
-public class MainActivity extends Activity
+public class MainActivity extends ActionBarActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         ImageView iv = new ImageView(this);
         iv.setImageResource(R.drawable.barutdlogo);
