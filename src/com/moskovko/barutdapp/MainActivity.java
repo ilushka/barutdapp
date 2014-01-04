a614d2cf2dc23b818709b6e7264403e9013e8ccf
ilya moskovko <ilya@iswifter.net>
2014-01-03 23:53:15 -0800

    made it so the main view slides away/toward from drawer

--- src/com/moskovko/barutdapp/MainActivity.java
+++ src/com/moskovko/barutdapp/MainActivity.java
@@ -1,19 +1,28 @@
 package com.moskovko.barutdapp;
 
 //import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.DrawerLayout.DrawerListener;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.animation.TranslateAnimation;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.widget.AbsoluteLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.view.Window;
-import android.support.v4.widget.DrawerLayout;
-import android.view.ViewGroup.LayoutParams;
-import android.view.View;
-import android.view.Gravity;
-import android.support.v4.widget.DrawerLayout.DrawerListener;
 
 public class MainActivity extends ActionBarActivity
 {
+    private static final String TAG = "MainActivity";
+    private static boolean DEBUG = true;
+
+    private View mMainView;
+    private View mDrawerView;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -23,34 +32,45 @@ public class MainActivity extends ActionBarActivity
 
         ImageView iv = new ImageView(this);
         iv.setImageResource(R.drawable.barutdlogo);
-        iv.setLayoutParams(new DrawerLayout.LayoutParams(
-            LayoutParams.MATCH_PARENT,
-            LayoutParams.MATCH_PARENT));
-
-/*
- *        RelativeLayout.LayoutParams rlp = new RelativeLayout.LayoutParams(
- *            RelativeLayout.LayoutParams.FILL_PARENT,
- *            RelativeLayout.LayoutParams.FILL_PARENT);
- *
- *        RelativeLayout rl = new RelativeLayout(this);
- *        rl.addView(iv);
- *
- *        setContentView(rl);
- */
-        DrawerLayout.LayoutParams dlp = new DrawerLayout.LayoutParams(
-            600, LayoutParams.MATCH_PARENT);
-        dlp.gravity = Gravity.START;
+        iv.setLayoutParams(new AbsoluteLayout.LayoutParams(
+            LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 0, 0));
+        AbsoluteLayout al = new AbsoluteLayout(this);
+        al.setLayoutParams(new DrawerLayout.LayoutParams(
+            LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        al.addView(iv);
+        this.mMainView = iv;
 
         View v = new View(this);
-        v.setLayoutParams(dlp);
+        v.setLayoutParams(new DrawerLayout.LayoutParams(
+            600, LayoutParams.MATCH_PARENT, Gravity.START));
         v.setBackgroundColor(0xFFFF0000);
+        this.mDrawerView = v;
 
         DrawerLayout dl = new DrawerLayout(this);
         dl.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, Gravity.START);
         dl.setLayoutParams(new LayoutParams(
             LayoutParams.FILL_PARENT,
             LayoutParams.FILL_PARENT));
-        dl.addView(iv);
+        dl.setDrawerListener(new DrawerListener() {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                if (DEBUG) Log.d(TAG, "onDrawerSlide: slideOffset: " + slideOffset);
+                slideMainView(slideOffset);
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+            }
+        });
+        dl.addView(al);
         dl.addView(v);
 
         setContentView(dl);
@@ -58,5 +78,17 @@ public class MainActivity extends ActionBarActivity
         /*
          *setContentView(R.layout.main);
          */
+    }
+
+    private void slideMainView(float offset) {
+        int width = mDrawerView.getLayoutParams().width;
+        float delta = (width * offset);
+
+        if (DEBUG) Log.d(TAG, "slideMainView: width: " + width + " delta: " + delta);
+
+        AbsoluteLayout.LayoutParams lp = (AbsoluteLayout.LayoutParams)mMainView
+            .getLayoutParams();
+        lp.x = (int)delta;
+        mMainView.setLayoutParams(lp);
     }
 }
