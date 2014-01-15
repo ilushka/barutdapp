38edaf768f6f8a6f583ae5f5728f3349ac691858
ilya moskovko <ilya@iswifter.net>
2014-01-14 21:14:08 -0800

    -- added internet permission. -- finished initial request logic. -- added support for RecieveResult to pass the response from the request.

--- src/com/moskovko/barutdapp/GetGamesRequest.java
+++ src/com/moskovko/barutdapp/GetGamesRequest.java
@@ -1,29 +1,78 @@
 package com.moskovko.barutdapp;
 
 import android.util.Log;
+import android.os.AsyncTask;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.StringBuilder;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.IOException;
+import android.os.ResultReceiver;
+import android.os.Bundle;
 
-public class GetGamesRequest extends AsyncTask<URL, Integer, void> {
+public class GetGamesRequest extends AsyncTask<URL, Integer, Void> {
     static private final String TAG = "ScheduleRequest";
 
+    static public final String REQUEST_RESPONSE = "RequestResponse";
+
+    private ResultReceiver mResultReceiver;
+    private String mResponse;
+
+    public GetGamesRequest(ResultReceiver rr) {
+        super();
+        mResultReceiver = rr;
+    }
+
     @Override
-    protected void doInBackground(URL... urls) {
-        HttpUrlConnection huc = (HttpUrlConnection)url[0].openConnection();
+    protected Void doInBackground(URL... urls) {
+        HttpURLConnection huc = null;
         try {
-            parseResponse(huc.getInputStream());
+            huc = (HttpURLConnection)urls[0].openConnection();
+            mResponse = readResponse(huc.getInputStream());
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             huc.disconnect();
         }
+        return null;
     }
 
     @Override
-    protected void onPostExecute(void) {
-        Log.d(TAG, "MONKEY: onPostExecute");
+    protected void onPostExecute(Void v) {
+        if (mResultReceiver != null) {
+            Bundle b = new Bundle();
+            b.putString(REQUEST_RESPONSE, mResponse);
+            mResultReceiver.send(1, b);
+        }
+    }
+
+    private String readResponse(InputStream is) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
     }
 
     private void parseResponse(InputStream is) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
 /*
         XmlPullParser xpp = Xml.newPullParser();
         xpp.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
--- src/com/moskovko/barutdapp/ScheduleFragment.java
+++ src/com/moskovko/barutdapp/ScheduleFragment.java
@@ -9,10 +9,33 @@ import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.view.LayoutInflater;
 import android.widget.ScrollView;
+import java.net.URL;
+import java.net.MalformedURLException;
+import android.os.ResultReceiver;
+import android.os.Handler;
 
 public class ScheduleFragment extends BoxListFragment {
+    static private final String TAG = "ScheduleFragment";
+
     public ScheduleFragment() {
         super();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        try {
+            new GetGamesRequest(new ResultReceiver(new Handler()) {
+                @Override
+                protected void onReceiveResult(int resultCode, Bundle resultData) {
+                    String response = resultData.getString(GetGamesRequest.REQUEST_RESPONSE);
+                    Log.d(TAG, "MONKEY: onReceiveResult: " + response);
+                }
+            }).execute(new URL("http://www.brrtr.com/schedule.xml"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
